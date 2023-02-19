ba47feb5fb3a479579d9d91a9f348c576587b418
Tolga Durman <56696405+TolgaDurman@users.noreply.github.com>
2023-02-19 12:35:04 +0300

    renaming

--- Assets/Flyweight/Scripts/Data.cs
+++ Assets/Flyweight/Scripts/Data.cs
@@ -1,29 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Data
+namespace FlyweightPattern
 {
-    private int number1 = 1;
-    private int number2 = 1;
-    private int number3 = 1;
-    private int number4 = 1;
-    private int number5 = 1;
-    private int number6 = 1;
-    private int number7 = 1;
-    private int number8 = 1;
-    private int number9 = 1;
-    private int number10 = 1;
-    private double double1 = 1.23d;
-    private double double2 = 1.23d;
-    private double double3 = 1.23d;
-    private double double4 = 1.23d;
-    private double double5 = 1.23d;
-    private double double6 = 1.23d;
-    private double double7 = 1.23d;
-    private double double8 = 1.23d;
-    private double double9 = 1.23d;
-    private double double10 = 1.23d;
-    private float someFloat = 12345.6789f;
-    private string someText = "Hello World";
+    public class Data
+    {
+        #pragma warning disable
+        private int number1 = 1;
+        private int number2 = 1;
+        private int number3 = 1;
+        private int number4 = 1;
+        private int number5 = 1;
+        private int number6 = 1;
+        private int number7 = 1;
+        private int number8 = 1;
+        private int number9 = 1;
+        private int number10 = 1;
+        private double double1 = 1.23d;
+        private double double2 = 1.23d;
+        private double double3 = 1.23d;
+        private double double4 = 1.23d;
+        private double double5 = 1.23d;
+        private double double6 = 1.23d;
+        private double double7 = 1.23d;
+        private double double8 = 1.23d;
+        private double double9 = 1.23d;
+        private double double10 = 1.23d;
+        private float someFloat = 12345.6789f;
+        private string someText = "Hello World";
+        #pragma warning restore
+    }
 }
--- Assets/Flyweight/Scripts/StatsDisplayer.cs
+++ Assets/Flyweight/Scripts/StatsDisplayer.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System.Text;
 using TMPro;
@@ -9,64 +7,67 @@ using UnityEngine.Profiling;
 using UnityEditor;
 #endif
 
-[RequireComponent(typeof(TextMeshProUGUI))]
-public class StatsDisplayer : MonoBehaviour
+namespace FlyweightPattern
 {
-    private StringBuilder tx;
-    private TextMeshProUGUI displayText;
-
-    float updateInterval = 1.0f;
-    float lastInterval; // Last interval end time
-    float frames = 0; // Frames over current interval
-
-    float framesavtick = 0;
-    float framesav = 0.0f;
-
-    // Use this for initialization
-    void Start()
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class StatsDisplayer : MonoBehaviour
     {
-        displayText = GetComponent<TextMeshProUGUI>();
-        lastInterval = Time.realtimeSinceStartup;
-        frames = 0;
-        framesav = 0;
-        tx = new StringBuilder();
-        tx.Capacity = 200;
-        Screen.sleepTimeout = SleepTimeout.NeverSleep;
-    }
+        private StringBuilder tx;
+        private TextMeshProUGUI displayText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        ++frames;
+        float updateInterval = 1.0f;
+        float lastInterval; // Last interval end time
+        float frames = 0; // Frames over current interval
 
-        var timeNow = Time.realtimeSinceStartup;
+        float framesavtick = 0;
+        float framesav = 0.0f;
 
-        if (timeNow > lastInterval + updateInterval)
+        // Use this for initialization
+        void Start()
         {
-            float fps = frames / (timeNow - lastInterval);
-            float ms = 1000.0f / Mathf.Max(fps, 0.00001f);
-
-            ++framesavtick;
-            framesav += fps;
-            float fpsav = framesav / framesavtick;
-
-            tx.Length = 0;
-
-            tx.AppendFormat("Time : {0} ms     Current FPS: {1}     AvgFPS: {2}\nGPU memory : {3}    Sys Memory : {4}\n", ms, fps, fpsav, SystemInfo.graphicsMemorySize, SystemInfo.systemMemorySize)
-            .AppendFormat("TotalAllocatedMemory : {0}mb\nTotalReservedMemory : {1}mb\nTotalUnusedReservedMemory : {2}mb",
-            Profiler.GetTotalAllocatedMemoryLong() / 1048576f,
-            Profiler.GetTotalReservedMemoryLong() / 1048576f,
-            Profiler.GetTotalUnusedReservedMemoryLong() / 1048576f
-            );
-
-#if UNITY_EDITOR
-            tx.AppendFormat("\nDrawCalls : {0}\nUsed Texture Memory : {1}\nrenderedTextureCount : {2}", UnityStats.drawCalls, UnityStats.usedTextureMemorySize / 1048576, UnityStats.usedTextureCount);
-#endif
-
-            displayText.text = tx.ToString();
+            displayText = GetComponent<TextMeshProUGUI>();
+            lastInterval = Time.realtimeSinceStartup;
             frames = 0;
-            lastInterval = timeNow;
+            framesav = 0;
+            tx = new StringBuilder();
+            tx.Capacity = 200;
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
         }
 
+        // Update is called once per frame
+        void Update()
+        {
+            ++frames;
+
+            var timeNow = Time.realtimeSinceStartup;
+
+            if (timeNow > lastInterval + updateInterval)
+            {
+                float fps = frames / (timeNow - lastInterval);
+                float ms = 1000.0f / Mathf.Max(fps, 0.00001f);
+
+                ++framesavtick;
+                framesav += fps;
+                float fpsav = framesav / framesavtick;
+
+                tx.Length = 0;
+
+                tx.AppendFormat("Time : {0} ms     Current FPS: {1}     AvgFPS: {2}\nGPU memory : {3}    Sys Memory : {4}\n", ms, fps, fpsav, SystemInfo.graphicsMemorySize, SystemInfo.systemMemorySize)
+                .AppendFormat("TotalAllocatedMemory : {0}mb\nTotalReservedMemory : {1}mb\nTotalUnusedReservedMemory : {2}mb",
+                Profiler.GetTotalAllocatedMemoryLong() / 1048576f,
+                Profiler.GetTotalReservedMemoryLong() / 1048576f,
+                Profiler.GetTotalUnusedReservedMemoryLong() / 1048576f
+                );
+
+#if UNITY_EDITOR
+                tx.AppendFormat("\nDrawCalls : {0}\nUsed Texture Memory : {1}\nrenderedTextureCount : {2}", UnityStats.drawCalls, UnityStats.usedTextureMemorySize / 1048576, UnityStats.usedTextureCount);
+#endif
+
+                displayText.text = tx.ToString();
+                frames = 0;
+                lastInterval = timeNow;
+            }
+
+        }
     }
 }
