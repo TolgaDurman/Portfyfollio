bf81f090f9d45b3fb11ed35ef57ca1c5c1cc3fda
TolgaDurman <Durman.tolga@gmail.com>
2023-11-07 18:01:26 +0300

    singleton

---
+++ Assets/Patterns/Singleton/Scripts/Singleton.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace SingletonPattern
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    // Find the instance in the scene
+                    instance = FindObjectOfType<T>();
+
+                    // If there is no instance, create one
+                    if (instance == null)
+                    {
+                        // Create a new game object
+                        GameObject obj = new GameObject();
+                        obj.name = typeof(T).Name;
+                        instance = obj.AddComponent<T>();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
